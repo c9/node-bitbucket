c8da143e3cfadb4e39fbc032ce656f180eed752a
russjohnson09@gmail.com <russjohnson09@gmail.com>
2016-12-08 08:09:47 -0500

    log all with winston

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,62 +1,15 @@
 var port = process.env.PORT || 3000;
 var ip = process.env.IP || '0.0.0.0';
 var init = process.env.INIT || false;
+
+
 const NEXMO_API = process.env.NEXMO_API || '123';
 const NEXMO_SECRET = process.env.NEXMO_SECRET || '123';
 const low = require('lowdb');
-
-const db = low('db.json');
-const PROTOCOL = process.env.PROTOCOL || 'http';
-const ENDPOINT_PORT = process.env.ENDPOINT_PORT || port;
-const HOST = process.env.HOST || 'localhost';
-const BASE_URL = PROTOCOL + "://" + HOST + ':' + ENDPOINT_PORT;
-const CRON_TIMER_SECONDS = process.env.CRON_TIMER_SECONDS || 2;
-
-const MongoClient = require('mongodb').MongoClient;
-
-const MONGO_CONNECTION = process.env.MONGO_CONNECTION || 'mongodb://localhost:27017/voice';
-
-MongoClient.connect(MONGO_CONNECTION, function (err, db) {
-    var collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-        { a: 1 }, { a: 2 }, { a: 3 }
-    ], function (err, result) {
-        console.log(err);
-        console.log(result.ops);
-        console.log("Inserted 3 documents into the document collection");
-    });
-});
-
-
-
-if (init) {
-    console.log('start init');
-
-    db.defaults({ settings: {} })
-        .value();
-
-    db.set('settings.nexmo_api', NEXMO_API).value();
-    db.set('settings.nexmo_secret', NEXMO_SECRET).value();
-
-}
-
-console.log(db.getState());
-
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
-var morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 var winston = require('winston');
 var Papertrail = require('winston-papertrail').Papertrail;
-
-var fs = require('fs');
-var path = require('path');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// create a write stream (in append mode)
 var logger = new winston.Logger({
     transports: [
         new winston.transports.File({
@@ -84,6 +37,50 @@ var logger = new winston.Logger({
     exitOnError: false
 });
 
+const db = low('db.json');
+const PROTOCOL = process.env.PROTOCOL || 'http';
+const ENDPOINT_PORT = process.env.ENDPOINT_PORT || port;
+const HOST = process.env.HOST || 'localhost';
+const BASE_URL = PROTOCOL + "://" + HOST + ':' + ENDPOINT_PORT;
+const CRON_TIMER_SECONDS = process.env.CRON_TIMER_SECONDS || 2;
+
+const MongoClient = require('mongodb').MongoClient;
+
+const MONGO_CONNECTION = process.env.MONGO_CONNECTION || 'mongodb://localhost:27017/voice';
+
+MongoClient.connect(MONGO_CONNECTION, function (err, db) {
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        { a: 1 }, { a: 2 }, { a: 3 }
+    ], function (err, result) {
+        winston.info(err);
+        winston.info(JSON.stringify(result.ops));
+        winston.info("Inserted 3 documents into the document collection");
+    });
+});
+
+
+
+if (init) {
+    winston.info('start init');
+
+    db.defaults({ settings: {} })
+        .value();
+
+    db.set('settings.nexmo_api', NEXMO_API).value();
+    db.set('settings.nexmo_secret', NEXMO_SECRET).value();
+
+}
+
+winston.info(db.getState());
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var morgan = require('morgan');
+
 
 logger.stream = {
     write: function (message, encoding) {
@@ -91,8 +88,12 @@ logger.stream = {
     }
 };
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// create a write stream (in append mode)
+
 // setup the logger
-app.use(morgan('combined', { stream: logger.stream }))
+app.use(morgan('combined', { stream: logger.stream }));
 
 
 var opts = { db: db, app: app };
