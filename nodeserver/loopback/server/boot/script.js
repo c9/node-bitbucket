5895c9291394dae4d364a573c3d8efbdc8532a8e
russjohnson09@gmail.com <russjohnson09@gmail.com>
2016-12-27 07:55:16 -0500

    added acls and default admin

---
+++ nodeserver/loopback/server/boot/script.js
@@ -0,0 +1,100 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-access-control
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+module.exports = function (app) {
+    var User = app.models.User;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+    var Team = app.models.Team;
+
+    // Role.destroyAll({}, function () {
+        // User.destroyAll({}, function () {
+            User.find({ where: { username: 'admin' } }, function (err, users) {
+                console.log(users);
+                if (!users[0]) {
+                    User.create({
+                        username: 'admin', email: 'admin@coderuss.com',
+                        password: 'admin'
+                    }, function (err, user) {
+                        if (err) throw err;
+                        console.log(users);
+                        //create the admin role
+                        Role.create({
+                            name: 'admin'
+                        }, function (err, role) {
+                            if (err) throw err;
+
+                            console.log('Created role:', role);
+
+                            //make bob an admin
+                            role.principals.create({
+                                principalType: RoleMapping.USER,
+                                principalId: user.id
+                            }, function (err, principal) {
+                                if (err) throw err;
+
+                                console.log('Created principal:', principal);
+                            });
+                        });
+                    })
+                }
+            // })
+        // })
+
+
+        // User.upsert([
+        //     { username: 'John', email: 'john@doe.com', password: 'opensesame' },
+        //     { username: 'Jane', email: 'jane@doe.com', password: 'opensesame' },
+        //     { username: 'Bob', email: 'bob@projects.com', password: 'opensesame' }
+        // ], function (err, users) {
+        //     if (err) throw err;
+
+        // console.log('Created users:', users);
+
+        // // create project 1 and make john the owner
+        // users[0].projects.create({
+        //   name: 'project1',
+        //   balance: 100
+        // }, function(err, project) {
+        //   if (err) throw err;
+
+        //   console.log('Created project:', project);
+
+        //   // add team members
+        //   Team.create([
+        //     {ownerId: project.ownerId, memberId: users[0].id},
+        //     {ownerId: project.ownerId, memberId: users[1].id}
+        //   ], function(err, team) {
+        //     if (err) throw err;
+
+        //     console.log('Created team:', team);
+        //   });
+        // });
+
+        // //create project 2 and make jane the owner
+        // users[1].projects.create({
+        //   name: 'project2',
+        //   balance: 100
+        // }, function(err, project) {
+        //   if (err) throw err;
+
+        //   console.log('Created project:', project);
+
+        //   //add team members
+        //   Team.create({
+        //     ownerId: project.ownerId,
+        //     memberId: users[1].id
+        //   }, function(err, team) {
+        //     if (err) throw err;
+
+        //     console.log('Created team:', team);
+        //   });
+        // });
+
+
+    });
+
+    // });
+};
