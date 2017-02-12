83774f5f59aa6b33803240a8e849f4af8b441554
russjohnson09@gmail.com <russjohnson09@gmail.com>
2017-02-12 11:33:54 -0500

    removed unused

--- nodeserver/public/l/js/routers/router.js
+++ nodeserver/public/l/js/routers/router.js
@@ -6,21 +6,21 @@ var app = app || {};
 
 	// Todo Router
 	// ----------
-	var TodoRouter = Backbone.Router.extend({
-		routes: {
-			'*filter': 'setFilter'
-		},
+	// var TodoRouter = Backbone.Router.extend({
+	// 	routes: {
+	// 		'*filter': 'setFilter'
+	// 	},
 
-		setFilter: function (param) {
-			// Set the current filter to be used
-			app.TodoFilter = param || '';
+	// 	setFilter: function (param) {
+	// 		// Set the current filter to be used
+	// 		app.TodoFilter = param || '';
 
-			// Trigger a collection filter event, causing hiding/unhiding
-			// of Todo view items
-			// app.todos.trigger('filter');
-		}
-	});
+	// 		// Trigger a collection filter event, causing hiding/unhiding
+	// 		// of Todo view items
+	// 		// app.todos.trigger('filter');
+	// 	}
+	// });
 
-	app.TodoRouter = new TodoRouter();
-	Backbone.history.start();
+	// app.TodoRouter = new TodoRouter();
+	// Backbone.history.start();
 })();
