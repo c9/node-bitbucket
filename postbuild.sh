759befe4da4712d1ac1c2021a5a9f1fccbd55e74
russjohnson09@gmail.com <russjohnson09@gmail.com>
2017-02-16 22:27:45 -0500

    search build directory

--- postbuild.sh
+++ postbuild.sh
@@ -4,7 +4,11 @@ whoami
 which 123
 which frotz
 echo $PATH
+echo $PWD
+
+
 frotz
+
 
 sudo which frotz
 
--- prebuild.sh
+++ prebuild.sh
@@ -4,6 +4,10 @@ whoami
 which 123
 which frotz
 echo $PATH
+echo $PWD
+ls "$BUILD_DIR/.apt/usr/bin"
+ls 
 
+frotz
 
 sudo which frotz
