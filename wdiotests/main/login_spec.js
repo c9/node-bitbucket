020eaca0bd8abd6ba137f6c52b7b14ff7fdb44b6
Russ Johnson <russjohnson09@gmail.com>
2017-02-22 07:42:21 -0500

    rearrange test order

--- wdiotests/main/login_spec.js
+++ wdiotests/main/login_spec.js
@@ -8,19 +8,15 @@ describe('login page', function() {
     it('should be able to login', function () {
         browser.url(publicurl+'/login');
         // filtering property commands
-        $('input[name=\'username\']').setValue('admin12345');
-        $('input[name=\'password\']').setValue('admin12345');
+        $('input[name=\'username\']').setValue('admin123456');
+        $('input[name=\'password\']').setValue('admin123456');
 
         $('button').click();
 
 
         browser.waitForVisible('form#create-todo');
-        
-        expect(browser.isVisible('form#create-todo'),'todo form is visible').to.be.true;
 
-        var results = $$('form#create-todo').filter(function (form) {
-            return form.isVisible();
-        });
+        expect(browser.isVisible('form#create-todo'),'todo form is visible').to.be.true;
 
     });
 });
